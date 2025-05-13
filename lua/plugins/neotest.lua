1f3cff234a42f37410a053e2e9dacb8cbcdce5d1
jartek <2030949+jartek@users.noreply.github.com>
2025-05-13 16:36:55 +0530

    chore: update plugins

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -11,12 +11,11 @@ return {
 		config = function()
 			require("overseer").setup()
 			local neotest = require("neotest")
-
 			neotest.setup({
 				adapters = {
 					require("neotest-rspec")({
-						rspec_cmd = function(path)
-							return vim.tbl_flatten({
+						rspec_cmd = function()
+							return {
 								"docker-compose",
 								"exec",
 								"-it",
@@ -24,16 +23,13 @@ return {
 								"bundle",
 								"exec",
 								"rspec",
-							})
+							}
 						end,
-
 						formatter = "json",
-
 						transform_spec_path = function(path)
 							local prefix = require("neotest-rspec").root(path)
 							return string.sub(path, string.len(prefix) + 2, -1)
 						end,
-
 						results_path = "tmp/rspec.output",
 					}),
 				},
@@ -54,25 +50,20 @@ return {
 					force_default = true,
 				},
 			})
-
 			vim.keymap.set("n", "<leader>tl", function()
 				neotest.run.run()
 			end, { desc = "Run nearest test" })
-
 			vim.keymap.set("n", "<leader>tf", function()
 				neotest.run.run(vim.fn.expand("%"))
 			end, { desc = "Run current file" })
-
 			-- Optional: Open the output in a floating window
 			vim.keymap.set("n", "<leader>to", function()
 				neotest.output.open({ enter = true, auto_close = true, last_run = true })
 			end, { desc = "Show test output" })
-
 			-- Optional: Jump to next/previous failed test
 			vim.keymap.set("n", "[t", function()
 				neotest.jump.prev({ status = "failed" })
 			end, { desc = "Jump to previous failed test" })
-
 			vim.keymap.set("n", "]t", function()
 				neotest.jump.next({ status = "failed" })
 			end, { desc = "Jump to next failed test" })
